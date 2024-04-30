018fd9e946a50b958c9df3cc4907c31a6d910210
rjaibi <aymenrjaibi2301@gmail.com>
2024-04-30 12:05:12 +0200

    communication microservices with webclient

--- employee-service/src/main/java/com/demo/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/demo/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -12,6 +12,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
@@ -20,6 +21,10 @@ public class EmployeeServiceImpl implements EmployeeService {
     private final EmployeeRepository employeeRepository;
     private ModelMapper mapper;
     private RestTemplate restTemplate;
+    private WebClient webClient;
+
+
+
 
 
     @Override
@@ -34,15 +39,39 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public APIResponseDto getEmployeeById(Long employeeId) {
 
-        Employee employee = employeeRepository.findById(employeeId).orElseThrow();
+        Employee employee = findEmployeeByIdOrThrow(employeeId);
         // TODO CODE REFACTOR FOR CONST AND ALL THE METHOD GET EMPLOYEE
-        String departmentApiUrl = ApiConstants.DEPARTMENT_API_URL;
-        ResponseEntity<DepartmentDto> response = restTemplate.getForEntity(departmentApiUrl + employee.getDepartmentCode(), DepartmentDto.class);
-        DepartmentDto departmentDto = response.getBody();
-        EmployeeDto employeeDto =mapper.map(employee, EmployeeDto.class);
+
+        DepartmentDto departmentDto = getDepartmentDetailsWithWebClient(employee);
+
+        EmployeeDto employeeDto =mapEmployeeToDto(employee);
+
         APIResponseDto apiResponseDto = new APIResponseDto();
         apiResponseDto.setEmployeeDto(employeeDto);
         apiResponseDto.setDepartmentDto(departmentDto);
         return apiResponseDto;
     }
+
+    private Employee findEmployeeByIdOrThrow(Long employeeId){
+        return employeeRepository.findById(employeeId).orElseThrow(()-> new RuntimeException("Could not find employee with id " + employeeId));
+
+    }
+    private EmployeeDto mapEmployeeToDto(Employee employee) {
+        return mapper.map(employee, EmployeeDto.class);
+    }
+
+    private DepartmentDto getDepartmentDetailsWithRestTemplate(Employee employee){
+        String departmentApiUrl = ApiConstants.DEPARTMENT_API_URL;
+        ResponseEntity<DepartmentDto> response = restTemplate.getForEntity(departmentApiUrl + employee.getDepartmentCode(), DepartmentDto.class);
+       return  response.getBody();
+    }
+    private DepartmentDto getDepartmentDetailsWithWebClient(Employee employee){
+        String departmentApiUrl = ApiConstants.DEPARTMENT_API_URL;
+        DepartmentDto departmentDto = webClient.get().uri(departmentApiUrl+employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
+        return departmentDto;
+    }
+
 }
